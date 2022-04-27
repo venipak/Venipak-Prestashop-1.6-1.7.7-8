55f373601ed674777b0f8931492dfbb58f696b7e
Modestas Slivinskas <modestas.slivinskas@gmail.com>
2022-04-27 17:38:47 +0300

    added vendor

---
+++ mijoravenipak/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e9db29901097765a0c9e4c99008739a00e329842',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e9db29901097765a0c9e4c99008739a00e329842',
+            'dev_requirement' => false,
+        ),
+    ),
+);
